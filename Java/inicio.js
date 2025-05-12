712b331476d0e83eddfdb8399c9eb7580e5bde2e
Clevi257 <andreslechugo8@gmail.com>
2025-05-12 19:31:27 -0300

    asdasdasd

--- Java/inicio.js
+++ Java/inicio.js
@@ -2,3 +2,9 @@
 function redirecionar(destino) {
     window.location.href = "Computador.html";
 }
+
+function removerSplash() {
+    document.getElementById("tela").style.display = "none";
+    document.getElementById("conteudo").style.display = "block";
+}
+
