cf6df8267dd55b53a903830ca7fadf9188854b72
Gustavo-coder-cmyk <gustvmillo@gmail.com>
2025-05-15 22:15:09 -0300

    Update

--- Java/inicio.js
+++
@@ -1,9 +0,0 @@
-
-function redirecionar(destino) {
-    window.location.href = "Computador.html";
-}
-
-function removerSplash() {
-    document.getElementById("tela").style.display = "none";
-    document.getElementById("conteudo").style.display = "block";
-}
